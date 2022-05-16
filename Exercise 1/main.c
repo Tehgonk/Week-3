856f0f1a4e87ba780ec33ce960676d12ed4d123d
D Geula <gonkhead112@gmail.com>
2022-05-16 21:35:58 +0100

    Initial commit

---
+++ Exercise 1/main.c
@@ -0,0 +1,17 @@
+/* Exercise 1 - Smallest Number in Array
+ * Author: Daniel Geula
+ * Date: 16/05/2022
+ * Version: 1.0
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+
+    
+
+  return 0;
+  
+}
