16c4ff1bb252a7f1c37981139e2beeec842e3ed9
D Geula <gonkhead112@gmail.com>
2022-05-17 00:31:56 +0100

    Version 1
    Probably final code. Removed any test code and finalised comments.

--- Exercise 1/main.c
+++ Exercise 1/main.c
@@ -10,32 +10,32 @@
 
 int main() {
 
-    int arr[20], n, min;
+    int arr[20], userNum, min;
 
+    // Prompt user for value of n, save as n
     printf("\nEnter a number: ");
-    scanf("%d", &n);
+    scanf("%d", &userNum);
     
     // Seed random number generator
     srand(time(NULL));
     
-    // Fill array with random number between 0 and N
+    // Fill each element of array with random number between 0 and n
     for(int i = 0; i < 20; ++i) {
-        arr[i] = rand() % (n + 1);
+        arr[i] = rand() % (userNum + 1);
     }
 
+    // Assign first value in array to min
     min = arr[0];
+
+    // Loop through array and find smallest number, save as min
     for(int i = 0; i < 20; i++) {
         if(arr[i] < min) {
             min = arr[i];
         }
     }
-
-    // For testing purposes
-    for(int i = 0; i < 20; i++) {
-        printf("%d ", arr[i]);
-    }
     
-    printf("Smallest: %d", min);
+    // Output value of min
+    printf("\nSmallest: %d\n", min);
     
 
   return 0;
