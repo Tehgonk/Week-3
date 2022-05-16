d19fccde4d9cddffea0f6281fa6caf79b1927099
D Geula <gonkhead112@gmail.com>
2022-05-16 22:35:04 +0100

    Fix missing '&'
    Value of n wasn't being saved which meant array was being filled with garbage.

--- Exercise 1/main.c
+++ Exercise 1/main.c
@@ -13,7 +13,7 @@ int main() {
     int arr[20], n, min;
 
     printf("\nEnter a number: ");
-    scanf("%d", n);
+    scanf("%d", &n);
     
     // Seed random number generator
     srand(time(NULL));
@@ -23,7 +23,7 @@ int main() {
     }
 
     for (int i = 0; i < 20; i++) {
-        printf("%d", arr[i]);
+        printf("%d ", arr[i]);
     }
     
 
