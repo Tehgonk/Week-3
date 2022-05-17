85d950c20d71b4ce8ce1819b459152897a895b6f
D Geula <gonkhead112@gmail.com>
2022-05-17 19:10:15 +0100

    Initial release
    Removed system("cls"). It was counter productive because I needed to be able to copy the output. Other very minor fixes / style changes.

--- Exercise 2/main.c
+++ Exercise 2/main.c
@@ -1,33 +1,39 @@
 /* Exercise 2 - Finding the Instances of a Number in an Array
  * Author: Daniel Geula
  * Date: 17/05/2022
- * Version: 
+ * Version: 1.0
  */
 
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
 
+    // initialize `count` to 0
     int userNum, count = 0, numArr[20];
-    char numStrings[20][20] = {"first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth", "thirteenth", "fourteenth", "fifteenth", "sixteenth", "seventeenth", "eighteenth", "nineteenth", "twentieth"};
+    char numStrings[20][20] = {"First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eighth", "Ninth", "Tenth", "Eleventh", "Twelfth", "Thirteenth", "Fourteenth", "Fifteenth", "Sixteenth", "Seventeenth", "Eighteenth", "Nineteenth", "Twentieth"};
 
-    for(int i = 0; i < 20; ++i) {
-        printf("\nEnter %s number: ", numStrings[i]);
+    // prompt user for twenty numbers, store in `numArr`
+    printf("\nEnter 20 numbers\n");
+    for (int i = 0; i < 20; ++i) 
+    {
+        printf("%s number: ", numStrings[i]);
         scanf("%d", &numArr[i]);
-        system("cls");
     }
 
-    printf("Now enter a random number: ");
-    scanf("%d", userNum);
+    // prompt user for number to search for in `numArr`
+    printf("\n\nEnter a number to search: ");
+    scanf("%d", &userNum);
 
-    for(int i = 0; i < 20; ++i) {
-        if(userNum == numArr[i]) {
-            count++;
+    for (int i = 0; i < 20; ++i)
+    {
+        if (numArr[i] == userNum)
+        {
+            count++; // increment `count` if number is found
         }
     }
 
-    printf("The number %d appears %d times in the array", userNum, count);
+    // output the number of times `userNum` was found in `numArr`
+    printf("\nThe number %d appears %d times in the array\n", userNum, count);
 
   return 0;
   
