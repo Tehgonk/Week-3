213e1837fe20700438e45909e5b15965c1702e07
D Geula <gonkhead112@gmail.com>
2022-05-17 17:34:30 +0100

    Test finding number of instances

--- Exercise 2/main.c
+++ Exercise 2/main.c
@@ -9,7 +9,7 @@
 
 int main() {
 
-    int userNum, count, numArr[20];
+    int userNum, count = 0, numArr[20];
     char numStrings[20][20] = {"first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth", "thirteenth", "fourteenth", "fifteenth", "sixteenth", "seventeenth", "eighteenth", "nineteenth", "twentieth"};
 
     for(int i = 0; i < 20; ++i) {
@@ -17,6 +17,17 @@ int main() {
         scanf("%d", &numArr[i]);
         system("cls");
     }
+
+    printf("Now enter a random number: ");
+    scanf("%d", userNum);
+
+    for(int i = 0; i < 20; ++i) {
+        if(userNum == numArr[i]) {
+            count++;
+        }
+    }
+
+    printf("The number %d appears %d times in the array", userNum, count);
 
   return 0;
   
