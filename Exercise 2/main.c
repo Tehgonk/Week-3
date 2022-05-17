43910be19f6af9128909be185a0d9e83b461c668
D Geula <gonkhead112@gmail.com>
2022-05-17 16:30:37 +0100

    Initial commit

---
+++ Exercise 2/main.c
@@ -0,0 +1,15 @@
+/* Exercise 2 - Finding the Instances of a Number in an Array
+ * Author: Daniel Geula
+ * Date: 17/05/2022
+ * Version: 
+ */
+
+#include <stdio.h>
+
+int main() {
+
+  
+
+  return 0;
+  
+}
