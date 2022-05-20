02a165476d45db20f50fb0d86412ff35a82eae29
D Geula <gonkhead112@gmail.com>
2022-05-20 16:50:17 +0100

    Fix user input / error checking

--- Exercise 4/main.c
+++ Exercise 4/main.c
@@ -12,13 +12,13 @@ int main() {
 
   // prompt user for two integers between 1-100, save as userNum1, userNum2
   printf("\nEnter two integer values between 0-100: ");
-  scanf("%d %d", userNum1, userNum2);
+  scanf("%d %d", &userNum1, &userNum2);
 
   // test that userNum1, userNum2 are in the range 0-100, else output error
   if (userNum1 < 0 || userNum1 > 100 || userNum2 < 0 || userNum2 > 100)
   {
-      printf("\nInvalid input. Values must be between 1-100.\n")
-      break;
+      printf("\nInvalid input. Values must be between 1-100.\n");
+      return 0;
   }
   
     printf("test");
