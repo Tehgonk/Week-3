16021332b053ea2ca8a68dd0314b4ec38a953e65
D Geula <gonkhead112@gmail.com>
2022-05-20 16:58:45 +0100

    Test find difference

--- Exercise 4/main.c
+++ Exercise 4/main.c
@@ -8,7 +8,7 @@
 
 int main() {
 
-  int userNum1, userNum2;
+  int userNum1, userNum2, diff;
 
   // prompt user for two integers between 1-100, save as userNum1, userNum2
   printf("\nEnter two integer values between 0-100: ");
@@ -20,8 +20,19 @@ int main() {
       printf("\nInvalid input. Values must be between 1-100.\n");
       return 0;
   }
+
+  // find difference between userNum1, useNum2, save as diff
+  if (userNum > userNum2)
+  {
+      diff = userNum1 - userNum2;
+  } else
+  {
+      diff = userNum2 - userNum1;
+  }
   
-    printf("test");
+  // test print
+  printf("diff: %d, n1: %d, n2: %d", diff, userNum1, userNum2);
+  
 
   return 0;
   
