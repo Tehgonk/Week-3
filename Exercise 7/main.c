5bf69d97c75d2e1440f28f74bc02c53b43286e16
D Geula <gonkhead112@gmail.com>
2022-05-26 21:54:44 +0100

    swap arrays

--- Exercise 7/main.c
+++ Exercise 7/main.c
@@ -11,13 +11,14 @@ int main() {
     // size of M and N
     int m, n, t;
 
-    printf("Enter the size of the array (m, n): ");
+    printf("Enter the size of the array (rows, columns): ");
     scanf("%d %d", &m, &n);
 
-    // create and populate two 2d arrays of size m, n
+    // create two 2d arrays of size m, n
     int arr1[m][n], arr2[m][n];
 
-    printf("\n\nEnter values for array 1\n");
+    // populate first array
+    printf("\nEnter values for array 1\n");
     for (int i = 0; i < m; ++i)
     {
         for (int j = 0; j < n; ++j)
@@ -28,13 +29,51 @@ int main() {
         
     }
 
-    printf("\n\nEnter values for array 2\n");
+    // populate second array
+    printf("\nEnter values for array 2\n");
     for (int i = 0; i < m; ++i)
     {
         for (int j = 0; j < n; ++j)
         {
             printf("%d of %d: ", (i * n) + (j + 1), (m * n));
             scanf("%d", &arr2[i][j]);
+        }
+        
+    }
+
+    // create swap array
+    int swapArr[m][n];
+
+    // swap first and second arrays
+    for (int i = 0; i < m; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            swapArr[i][j] = arr2[i][j];
+            arr2[i][j] = arr1[i][j];
+            arr1[i][j] = swapArr[i][j]; 
+        }
+        
+    }
+
+    // print first array (swapped)
+    printf("\nSwapped values of array 1:\n");
+    for (int i = 0; i < m; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            printf("%d ", arr1[i][j]);
+        }
+        
+    }
+
+    // print second array (swapped)
+    printf("\n\nSwapped values of array 2:\n");
+    for (int i = 0; i < m; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            printf("%d ", arr2[i][j]);
         }
         
     }
