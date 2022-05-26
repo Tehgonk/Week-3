95ac31540047cf1f5e26a956ef98fa03bbc95a9d
D Geula <gonkhead112@gmail.com>
2022-05-26 19:43:43 +0100

    initial commit

---
+++ Exercise 7/main.c
@@ -0,0 +1,15 @@
+/* Exercise 7 - 2D Array Swap
+ * Author: Daniel Geula
+ * Date: 26/05/22
+ * Version: 1.0
+ */
+
+#include <stdio.h>
+
+int main() {
+
+    //CODE HERE
+
+    return 0;
+    
+    }
