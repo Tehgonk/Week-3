13624e51712ff8890027b4ac925d9dcbad18f092
D Geula <gonkhead112@gmail.com>
2022-05-26 19:45:37 +0100

    create and populate arrays

--- Exercise 7/main.c
+++ Exercise 7/main.c
@@ -8,8 +8,38 @@
 
 int main() {
 
-    //CODE HERE
+    // size of M and N
+    int m, n, t;
 
+    printf("Enter the size of the array (m, n): ");
+    scanf("%d %d", &m, &n);
+
+    // create and populate two 2d arrays of size m, n
+    int arr1[m][n], arr2[m][n];
+
+    printf("\n\nEnter values for array 1\n");
+    for (int i = 0; i < m; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            printf("%d of %d: ", (i * n) + (j + 1), (m * n));
+            scanf("%d", &arr1[i][j]);
+        }
+        
+    }
+
+    printf("\n\nEnter values for array 2\n");
+    for (int i = 0; i < m; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            printf("%d of %d: ", (i * n) + (j + 1), (m * n));
+            scanf("%d", &arr2[i][j]);
+        }
+        
+    }
+
+    
     return 0;
     
     }
